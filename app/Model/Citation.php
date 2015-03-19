ccfe2b95c886e8f9361e71bfa3126972ef7ab209
Stuart Chalk <schalk@unf.edu>
2015-03-19 08:38:09 -0400

    Code cleanup

--- app/Model/Citation.php
+++ app/Model/Citation.php
@@ -1,22 +1,28 @@
 <?php
-
 App::uses('AppModel', 'Model');
+
+/**
+ * Class Citation
+ * Model for the citations table
+ */
 class Citation extends AppModel
 {
-	
 	public $hasMany='System';
 	
-	public $hasAndBelongsToMany = array(
+	public $hasAndBelongsToMany = [
 		'Author' =>
-			array(
+			[
 				'className' => 'Author',
 				'joinTable' => 'authors_citations',
 				'foreignKey' => 'citation_id',
 				'associationForeignKey' => 'author_id',
 				'unique' => true
-				)
-		);
+				]
+		];
 
-	public $virtualFields = array('cite' => 'CONCAT(Citation.journal," ",Citation.year,", ",Citation.volume,", ",Citation.firstpage)');
+	public $virtualFields = [
+        'cite'  => 'CONCAT(Citation.journal," ",Citation.year,", ",Citation.volume,", ",Citation.firstpage)',
+        'first' => 'UPPER(SUBSTR(Citation.journal,1,1))'
+        ];
 	
 }
